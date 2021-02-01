451c4c4154a2c9bfe96ec5ed6ee27256ffb4c257
AhmedBarre10 <barreahmed600@gmail.com>
2021-02-01 12:28:03 -0500

    s

--- authRoutes/Login.js
+++ authRoutes/Login.js
@@ -24,6 +24,16 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+router.get('/email', async (req, res) => {
+  try {
+    const user = await User.find().select('-password -name -date -id -_id -__v');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 /// send user id to email address
 // get put user.password = hashed then save
 
